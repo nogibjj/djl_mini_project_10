8c6f0ddad830fb628be8526961916a79b60e892a
dani-jimlar <143829673+dani-jimlar@users.noreply.github.com>
2023-11-06 05:48:50 +0000

    update main

--- main.py
+++ main.py
@@ -26,21 +26,21 @@ def main():
     # Register DataFrame as a temporary SQL table/view
     imecas_df.createOrReplaceTempView("imecas")
 
-    # Step 4: Perform a Spark SQL query
+    # Spark SQL query
     sql_query_string = "zona = 'NE'"
     result_df = sql_query(imecas_df, sql_query_string)
 
     # Print out the result DataFrame
     result_df.show()
 
-    # Step 5: Perform data transformation
+    #  data transformation
     column_of_interest = "zona"
     target_value = "CE"
     transformed_df = perform_data_transformation(
         imecas_df, column_of_interest, target_value
     )
 
-    # Combine the results into the report_content
+    # Build report
     report_content = "SQL Query Result (NE zone):\n"
     report_content += f"Rows: {result_df.count()}\n\n"
     report_content += "Transformed DataFrame (CE zone):\n"
