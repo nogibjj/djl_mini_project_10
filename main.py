9b40d5f55e0c1d5e2f27d80bae0443cd7894bb3e
dani-jimlar <143829673+dani-jimlar@users.noreply.github.com>
2023-11-06 05:37:39 +0000

    update main and report

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """
-Main ci
+Main 
 """
 
 from my_lib.lib import (
@@ -16,7 +16,7 @@ def main():
     spark = initialize_spark()
 
     #  Load the imecas dataset
-    file_path = "https://raw.githubusercontent.com/jjsantos01/aire_cdmx/master/datos/contaminantes_2019-05-16.csv",
+    file_path = "contaminantes.cvs"
 
     imecas_df = read_csv(spark, file_path)
 
@@ -27,24 +27,24 @@ def main():
     imecas_df.createOrReplaceTempView("imecas")
 
     # Step 4: Perform a Spark SQL query
-    sql_query_string = "zona = 'Sam Smith'"
-    result_df = sql_query(songs_df, sql_query_string)
+    sql_query_string = "zona = 'NE'"
+    result_df = sql_query(imecas_df, sql_query_string)
 
     # Print out the result DataFrame
     result_df.show()
 
     # Step 5: Perform data transformation
-    column_of_interest = "artist"
-    target_value = "Maroon 5"
+    column_of_interest = "zona"
+    target_value = "CE"
     transformed_df = perform_data_transformation(
-        songs_df, column_of_interest, target_value
+        imecas_df, column_of_interest, target_value
     )
 
     # Combine the results into the report_content
-    report_content = "SQL Query Result:\n"
-    report_content += f"Number of rows: {result_df.count()}\n\n"
-    report_content += "Transformed DataFrame:\n"
-    report_content += f"Number of rows: {transformed_df.count()}\n"
+    report_content = "SQL Query Result (NE zone):\n"
+    report_content += f"Rows: {result_df.count()}\n\n"
+    report_content += "Transformed DataFrame (CE zone):\n"
+    report_content += f"Rows: {transformed_df.count()}\n"
 
     # Step 6: Save summary report
     report_file_path = "report.txt"
